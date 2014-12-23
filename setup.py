d1634114cce5a5dcd6afe3a5b966b64c7a805cfd
Rense VanderHoek <vanderhoek@gmail.com>
2014-12-23 13:18:49 +0100

    Initial commit 1.0b
    Initial commit for version 1.0b

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PyRDW',
+    version='1.0b',
+    description='Validate',
+    url='http://github.com/rense/pyrdw',
+    author='Rense VanderHoek',
+    author_email='vanderhoek@gmail.com',
+    license='MIT',
+    packages=['PyRDW'],
+    zip_safe=False
+)
