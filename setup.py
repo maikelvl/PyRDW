a73cfca851e47fdd57f0528a57420fc964e380a0
Rense VanderHoek <vanderhoek@gmail.com>
2014-12-23 14:28:14 +0100

    setup.py fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='PyRDW',
-    version='1.0b',
+    version='1.0',
     description='PyRDW gives a basic way of fetching licensenumber-data from the RDW, the dutch department of road transport.',
     long_description=readme(),
     url='http://github.com/rense/pyrdw',
@@ -18,10 +18,10 @@ setup(
     zip_safe=False,
     include_package_data=True,
     classifiers=[
-        'Classifier: Development Status :: 4 - Beta',
-        'Classifier: Intended Audience :: Developers',
-        'Classifier: License :: OSI Approved :: MIT License',
-        'Classifier: Natural Language :: Dutch',
-        'Classifier: Programming Language :: Python :: 2 :: Only'
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Dutch',
+        'Programming Language :: Python :: 2 :: Only'
     ],
 )
